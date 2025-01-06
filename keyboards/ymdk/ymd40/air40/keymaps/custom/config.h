e4540a93e4d63f7c47ae535c80ea6c1c48da9ff6
NeonDactyl <treyw05@gmail.com>
2025-01-06 13:58:39 -0600

    finished layers
    finished layers
    removed pretty borders :(

---
+++ keyboards/ymdk/ymd40/air40/keymaps/custom/config.h
@@ -0,0 +1,52 @@
+#pragma once
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define QUICK_TAP_TERM 0
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+
+#undef MOUSEKEY_DECELERATED_SPEED
+#define MOUSEKEY_DECELERATED_SPEED 1
+
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 0
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 3
+
+#undef MOUSEKEY_MOVE_DELTA
+#define MOUSEKEY_MOVE_DELTA 2
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 10
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 90
+
+#undef MOUSEKEY_FRICTION
+#define MOUSEKEY_FRICTION 255
+
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY 40
+
+#undef MOUSEKEY_WHEEL_INTERVAL
+#define MOUSEKEY_WHEEL_INTERVAL 25
+
+#undef MOUSEKEY_WHEEL_DELTA
+#define MOUSEKEY_WHEEL_DELTA 1
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 10
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 255
+
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_16BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
