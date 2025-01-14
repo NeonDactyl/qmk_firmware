e40f0865b802407a0153f4c4e433e30141ac400a
NeonDactyl <treyw05@gmail.com>
2025-01-13 23:38:21 -0600

    updated symbols

--- keyboards/ymdk/ymd40/air40/keymaps/custom/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/custom/keymap.c
@@ -57,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [_FSYM] = LAYOUT_ortho_4x12(
         _____      , KC_EXLM    , KC_AT      , KC_HASH    , KC_DLR     , KC_PERC    , KC_CIRC    , KC_AMPR    , KC_ASTR    , KC_LPRN    , KC_RPRN    , KC_BSLS    ,
-        _____      , KC_F1      , KC_F2      , KC_F3      , KC_F4      , KC_F5      , KC_F6      , KC_UNDS    , KC_PLUS    , KC_LCBR    , KC_RCBR    , KC_SLSH    ,
-        _____      , KC_F7      , KC_F8      , KC_F9      , KC_F10     , KC_F11     , KC_F12     , KC_MINS    , KC_EQUAL   , KC_LBRC    , KC_RBRC    , _____      ,
+        _____      , KC_F1      , KC_F2      , KC_F3      , KC_F4      , KC_F5      , KC_F6      , KC_UNDS    , KC_PLUS    , KC_LCBR    , KC_RCBR    , KC_QUOTE   ,
+        _____      , KC_F7      , KC_F8      , KC_F9      , KC_F10     , KC_F11     , KC_F12     , KC_MINS    , KC_EQUAL   , KC_LBRC    , KC_RBRC    , KC_SLSH    ,
         _____      , _____      , _____      , _____      , _____      , _____      , _____      , _____      , _____      , _____      , _____      , _____
     ),
     [_NUMNAV] = LAYOUT_ortho_4x12(
